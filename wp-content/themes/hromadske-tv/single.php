a8c4c14d0138ddb99d717deff7c8dc2e995de1f2
marinka1809 <marina.paliy@n3wnormal.com>
2017-11-22 00:22:22 +0200

    Change plugin by share button

--- wp-content/themes/hromadske-tv/single.php
+++ wp-content/themes/hromadske-tv/single.php
@@ -16,30 +16,17 @@ get_header(); ?>
 
         <?php ?>
 
-        <?php add_filter('the_content', 'wpse_ad_content');
+<!--        --><?php //add_filter('the_content', 'wpse_ad_content');
 
-        function wpse_ad_content($content)
-        {
-            $tag_block ='';
-            if( has_term('', 'post_tag', $post->ID) ):
-                $tag_block = '<div class="tags-list">'  . get_the_term_list( $post->ID, 'post_tag', '', '', '' ) .'</div>';
-            elseif ( has_term('', 'stories-tags', $post->ID) ):
-                $tag_block.= '<div class="tags-list">';
-                $tag_block.= get_the_term_list( $post->ID, 'stories-tags', '', '', '' );
-                $tag_block.= '</div>';
-            elseif ( has_term('', 'episodes-tags', $post->ID) ):
-                $tag_block = '<div class="tags-list">' . get_the_term_list( $post->ID, 'episodes-tags', '', '', '' ) .'</div>';
-            elseif ( has_term('', 'production-tags', $post->ID) ):
-                $tag_block.= '<div class="tags-list">';
-                $tag_block.= get_the_term_list( $post->ID, 'production-tags', '', '', '' );
-                $tag_block.= '</div>';
-            endif;
-
-            $content = explode('<div class="getsocial gs-inline-group', $content, 2);
-            $new_content = $content[0] .$tag_block .'<div class="getsocial gs-inline-group'  .$content[1];
-
-        return $new_content;
-        }?>
+//        function wpse_ad_content($content)
+//        {
+//           
+//
+//            $content = explode('<div class="getsocial gs-inline-group', $content, 2);
+//            $new_content = $content[0] .$tag_block .'<div class="getsocial gs-inline-group'  .$content[1];
+//
+//        return $new_content;
+//        }?>
 
         <?php get_template_part( 'template-parts/content');?>
         <?php get_template_part( 'template-parts/related', get_post_type());?>
--- wp-content/themes/hromadske-tv/template-parts/content.php
+++ wp-content/themes/hromadske-tv/template-parts/content.php
@@ -9,11 +9,29 @@
 
 ?>
 
-
 	<div class="single-content">
 
 		<?php
 			the_content( );
+
+		$tag_block ='';
+		if( has_term('', 'post_tag', $post->ID) ):
+			$tag_block = '<div class="tags-list">'  . get_the_term_list( $post->ID, 'post_tag', '', '', '' ) .'</div>';
+		elseif ( has_term('', 'stories-tags', $post->ID) ):
+			$tag_block.= '<div class="tags-list">';
+			$tag_block.= get_the_term_list( $post->ID, 'stories-tags', '', '', '' );
+			$tag_block.= '</div>';
+		elseif ( has_term('', 'episodes-tags', $post->ID) ):
+			$tag_block = '<div class="tags-list">' . get_the_term_list( $post->ID, 'episodes-tags', '', '', '' ) .'</div>';
+		elseif ( has_term('', 'production-tags', $post->ID) ):
+			$tag_block.= '<div class="tags-list">';
+			$tag_block.= get_the_term_list( $post->ID, 'production-tags', '', '', '' );
+			$tag_block.= '</div>';
+		endif;
+
+		echo $tag_block;
 		?>
+		<!-- Go to www.addthis.com/dashboard to customize your tools -->
+		<div class="addthis_inline_share_toolbox_dyx4"></div>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
